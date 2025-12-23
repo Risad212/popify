25693b055398ed0fbf0a860ed5b837015a3c9b8d
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2025-12-23 21:22:00 +0600

    Update popup.php

--- view/frontend/popup.php
+++ view/frontend/popup.php
@@ -1,18 +1,24 @@
-<?php  
-  add_action('wp_footer', 'popify_frontend_popup');
+<?php
 
-
-  function popify_frontend_popup() { ?>
-   <div id="popify-popup" class="popify-popup">
-    <div class="popify-popup-content">
-      <span class="popify-popup-close">&times;</span>
-      <h2>Popify Text Preview</h2>
+function popify_frontend_popup() {
+  $popup_data = get_option( 'popify_option' );
+  var_dump( $popup_data );
+    ob_start();
+    ?>
+    <div id="popify-popup" class="popify-popup">
+        <div class="popify-popup-content" style="width: <?php echo $popup_data['width'];?>px; <?php echo $popup_data['height'];?>px">
+            <span class="popify-popup-close">&times;</span>
+            <h2 style="font-size: <?php echo $popup_data['font_size']; ?>px; color:<?php echo $popup_data['text_color'];?>">
+              <?php echo $popup_data['text']; ?>
+            </h2>
+        </div>
     </div>
-  </div>
+    <?php
+    return ob_get_clean();
+}
 
-  <?php
-  }
- ?>
+add_shortcode( 'popify_shortcode', 'popify_frontend_popup' );
+
 
 
 
