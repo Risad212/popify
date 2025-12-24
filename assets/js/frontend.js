b10057db2a0ba231e52329bda7300a82ac7c47f8
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2025-12-24 09:59:11 +0600

    Update frontend.js

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -2,9 +2,9 @@
 jQuery(window).on('load', function($){
     jQuery('#popify-popup').fadeIn();
     jQuery('.popify-popup-close, #popify-popup').on('click', function(e){
-        if(e.target !== this) return;
         jQuery('#popify-popup').fadeOut();
     });
 });
+
 
 
